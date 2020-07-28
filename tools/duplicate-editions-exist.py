790d69bba3c56c1046a206612e0ff4fc4aee241c
Mirek Długosz <mzalewsk@redhat.com>
2020-07-29 01:28:41 +0200

    Add simple PR checking tool and Actions workflow

---
+++ tools/duplicate-editions-exist.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+import yaml
+
+
+class DuplicateEditionError(Exception):
+    pass
+
+
+def parse_file_metadata(fileobj):
+    with open(fileobj) as fh:
+        front_matter = fh.read()
+    begin = 3
+    end = front_matter.index('---', begin)
+    front_matter = front_matter[begin:end]
+    metadata = yaml.safe_load(front_matter)
+    return metadata
+
+
+if __name__ == "__main__":
+    editions = []
+    for post in Path('content/posts/').glob('*.md'):
+        metadata = parse_file_metadata(post)
+        this_edition = (metadata['year'], metadata['edition'])
+        if this_edition in editions:
+            print(editions)
+            msg = (f"{post.name} contains duplicate edition id, "
+                   f"year: {this_edition[0]}, edition: {this_edition[1]}")
+            raise DuplicateEditionError(msg)
+        editions.append(this_edition)
