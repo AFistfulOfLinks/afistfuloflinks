95330ab462439252de61d4c0885168a4051bcfc8
Mirek Długosz <mzalewsk@redhat.com>
2020-07-30 12:18:17 +0200

    Add info about submitter to unclaimed-links output
    If submitter field is not present, it will see who commited file to git

--- tools/unclaimed-links.py
+++ tools/unclaimed-links.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import subprocess
+
 from helpers import HugoContent
 
 
@@ -19,9 +21,19 @@ def main():
 
     for path in unclaimed_links:
         metadata = page_content.get_metadata(path)
-        item = "{id}\nurl: {url}\ntitle: {title}"
+        if 'submitter' in metadata:
+            submitter = metadata['submitter']['name']
+        else:
+            process = subprocess.run(
+                ['git', 'log', '-s', '-n1', '--format=%an', path.as_posix()],
+                stdout=subprocess.PIPE
+            )
+            submitter = process.stdout.decode("UTF-8").strip()
+            submitter = f"{submitter} (from git)"
+        item = "{id}\nurl: {url}\ntitle: {title}\nsubmitter: {submitter}"
         item = item.format(
-            id=path.stem, url=metadata['source_url'], title=metadata['title']
+            id=path.stem, url=metadata['source_url'], title=metadata['title'],
+            submitter=submitter
         )
         result.append(item)
     print("\n\n\n".join(result))
